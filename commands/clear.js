fddedc8782a80e254ed5999ecf565e9625bb396d
Nick F <seven1993@live.com>
2023-06-05 09:26:39 -0400

    add Word2Vec model - AI

--- commands/clear.js
+++ commands/clear.js
@@ -1,13 +1,73 @@
-module.exports = function myClearFunction( messagePassed, passedItemArgs ) 
+module.exports = function myClearFunction( messagePassed, passedItemArgs ) {
+	try {
+		if ( passedItemArgs[0] === 'sender' ) 
+		{
+			// Delete all messages by a specific sender
+			const senderName = passedItemArgs.slice( 1 ).join( ' ' ).toLowerCase( );
+			messagePassed.channel.messages.fetch( ).then( ( messages ) => {
+				const senderMessages = messages.filter( ( message ) =>
+					message.author.username.toLowerCase( ) === senderName );
+
+				let deletedCount = 0;
+				senderMessages.forEach( ( message ) => {
+					if ( isNewerThanTwoWeeks( message ) ) 
+					{
+						message.delete( )
+						.then( ( ) => {
+							deletedCount++;
+							console.log( `Deleted message: ${message.content}` );
+						} )
+						.catch( ( error ) => {
+							console.error( `Error deleting message: ${message.content}` );
+							console.error( error );
+						} );
+					}
+				} );
+
+				messagePassed.channel.send( `Cleared ${deletedCount} messages by ${senderName}.` );
+			} );
+		} 
+		else if ( passedItemArgs.length > 0 && !isNaN( passedItemArgs[0] ) ) 
+		{
+			// Delete specific number of messages
+			const amount = parseInt( passedItemArgs[0] );
+			if ( amount < 1 || amount > 99 ) { return messagePassed.reply( 'Enter a number between 1 and 99.' ); }
+
+			messagePassed.channel.messages.fetch( ).then( ( messages ) => {
+				const messagesArray = Array.from( messages.values( ) );
+				const messagesToDelete = messagesArray.filter( ( message ) => isNewerThanTwoWeeks( message ) ).slice( 0, amount );
+				messagePassed.channel.bulkDelete(messagesToDelete).then(() => { messagePassed.channel.send( `Cleared ${amount} messages.` ); } )
+				.catch( ( error ) => { console.error( 'Error while deleting messages:', error ); } );
+			});
+		}
+		else 
+		{
+			// If no number is passed, delete messages newer than 2 weeks
+			messagePassed.channel.messages.fetch( ).then( ( messages ) => {
+				const messagesToDelete = messages.filter( ( message ) => isNewerThanTwoWeeks( message ) );
+				messagePassed.channel.bulkDelete( messagesToDelete ).then( ( ) => { messagePassed.channel.send( 'Cleared all messages newer than 2 weeks.' ); } );
+			} );
+		}
+	} 
+	catch ( error ) { messagePassed.channel.send( 'Clear Failed... ' ); }
+};
+
+function isNewerThanTwoWeeks( messagePassed02 ) 
+{
+	const twoWeeksAgo = new Date( ).getTime( ) - 14 * 24 * 60 * 60 * 1000;
+	return messagePassed02.createdTimestamp > twoWeeksAgo;
+}
+
+/* module.exports = function myClearFunction( messagePassed, passedItemArgs ) 
 {
 	try {
 		if ( passedItemArgs[0] === 'sender' ) 
 		{
 			// Delete all messages by a specific sender
-			const senderName = passedItemArgs.slice( 1 ).join( ' ' ).toLowerCase();
-			messagePassed.channel.messages.fetch().then( ( messages ) => {
+			const senderName = passedItemArgs.slice( 1 ).join( ' ' ).toLowerCase( );
+			messagePassed.channel.messages.fetch( ).then( ( messages ) => {
 				const senderMessages = messages.filter( ( message ) => 
-					message.author.username.toLowerCase() === senderName );
+					message.author.username.toLowerCase( ) === senderName );
 					messagePassed.channel.bulkDelete( senderMessages );
 					messagePassed.channel.send( `Cleared all messages by ${senderName}.` ); } );
 		} 
@@ -17,15 +77,15 @@ module.exports = function myClearFunction( messagePassed, passedItemArgs )
 			const amount = parseInt( passedItemArgs );
 			if ( amount < 1 || amount > 99 ) { return messagePassed.reply( 'Enter a number between 1 and 99.' ); }
       
-			messagePassed.channel.bulkDelete( amount + 1 ).then( () => {
+			messagePassed.channel.bulkDelete( amount + 1 ).then( ( ) => {
 				messagePassed.channel.send( `Cleared ${amount} messages.` ); } );
 		} 
 		else // If no number is passed, delete 99 messages
 		{
 			messagePassed.channel.messages.fetch( { limit: 99 } ).then( ( messages ) => {
-				messagePassed.channel.bulkDelete( messages ).then( () => {
+				messagePassed.channel.bulkDelete( messages ).then( ( ) => {
 					messagePassed.channel.send( 'Cleared all messages.' ); } ); } );
 		}
 	} 
 	catch ( error ) { messagePassed.channel.send( 'Clear Failed... ' ); }
-}
+} */
--- commands/sloot.js
+++ commands/sloot.js
@@ -1,5 +1,14 @@
 const { getRandomInt, theShuffler } = require( './helpers' );
+
 let loopTimer;
+let randHolder;
+
+const starz = [ 'apple', 'banana', 'carrot' ];
+  
+const extraOne = [ 'red', 'blue', 'green' ];
+  
+const extraTwo = [ '1', '2', '3' ];
+
 module.exports = async function mySlootFunction( messagePassed, passedItemArgs, google ) 
 {
 	try {
@@ -17,6 +26,7 @@ module.exports = async function mySlootFunction( messagePassed, passedItemArgs, 
 				clearInterval( loopTimer );
 				// Initialize loop counter
 				let loopCounter = 0;
+				let slootCounter = 0;
 				await clearPreviousMessages();
 				// Set an interval to loop the 'sloot' scraper every 91 seconds
 				loopTimer = setInterval( async () => {
@@ -26,9 +36,11 @@ module.exports = async function mySlootFunction( messagePassed, passedItemArgs, 
 						await clearPreviousMessages();
 						loopCounter = 0;
 					}
+					if ( slootCounter == starz.length ) { slootCounter = 0; }
 					// Call the 'sloot' scraper function
-					await mySlootScraper( messagePassed, google );
-					// Increment the loop counter
+					await mySlootScraper( messagePassed, google, slootCounter);
+					// Increment the loop counters
+					slootCounter++;
 					loopCounter++;
 				}, 91000 );
 				break;
@@ -51,46 +63,40 @@ module.exports = async function mySlootFunction( messagePassed, passedItemArgs, 
 	catch ( error ) { messagePassed.channel.send( 'Sloot Failed... ' ); }
 }
 
-const starz = [
-  '1', '2', '3',
-];
-
-const extraOne = [
-  '1', '2', '3',
-];
-
-const extraTwo = [
-	'1', '2', '3',
-];
-
 // Function to scrape for sloot images
-async function mySlootScraper( messagePassed, google ) 
+async function mySlootScraper( messagePassed, google, slootStarCount ) 
 {
 	// Generate random numbers and mix up the arrays
-	const rand = getRandomInt( starz.length );
-	const rand2 = getRandomInt( extraOne.length );
-	const rand3 = getRandomInt( extraTwo.length );
-	const rand4 = getRandomInt( 60 );
-	theShuffler( starz );
-	theShuffler( extraOne );
-	theShuffler( extraTwo );
+	let rand = getRandomInt( extraOne.length ) - 1;
+	do { rand = getRandomInt( extraOne.length ) - 1 } while (rand == randHolder);
+	const rand2 = getRandomInt( extraTwo.length ) - 1;
+	const rand3 = getRandomInt( 60 );
+	if ( slootStarCount == 0 ) 
+	{ 
+		let oldStarz = starz[0]
+		do { theShuffler( starz ); } while ( oldStarz == starz[0] );
+	}
+	if ( rand > 12) { theShuffler( extraOne ); }
+	else { theShuffler( extraTwo ); }
 
 	try {
 		// Search Google for images with query
-		const imageResults = await google.scrape( `${starz[rand]} ${extraOne[rand2]} ${extraTwo[rand3]} gif`, rand4 );
+		const imageResults = await google.scrape( `${starz[slootStarCount]} ${extraOne[rand]} ${extraTwo[rand2]} gif`, rand3 );
 		// Find the first image result that doesn't start with any of these domains
 		const imgResult = imageResults.find( ( result ) => { return !result.url.startsWith( 'https://cdn.' ) } );
 		// If there is an image result, send the image and a message about the search terms
 		if ( imgResult ) 
 		{
 			messagePassed.channel.send( imgResult.url );
-			messagePassed.channel.send( `I found this... ${starz[rand]}, ${extraOne[rand2]}` );
+			messagePassed.channel.send( `I found this... ${starz[slootStarCount]}, ${extraOne[rand]}` );
+			randHolder = rand;
+			console.log( slootStarCount + " " + rand + " " + rand3 );
 		} 
 		// If no image result is found, call the function recursively
-		else { mySlootScraper( messagePassed ); }
+		else { mySlootScraper( messagePassed, google, slootStarCount ); }
 	} 
 	// If there is an error, send an error message with the search terms
-	catch ( error ) { messagePassed.channel.send( `Failed... ${starz[rand]}, ${extraOne[rand2]}, ${x}` ); }
+	catch ( error ) { messagePassed.channel.send( `Failed... ${starz[slootStarCount]}, ${extraOne[rand]}, ${rand3}` ); }
 }
 
 /*
@@ -99,7 +105,7 @@ const cheerio = require( 'cheerio' );
 
 async function myHubScraper( messagePassed ) 
 {
-	const url = 'https://www.google.com/';
+	const url = 'https://www.google.com/gifs';
 	// Make a GET request to the URL using Axios
 	axios.get( url ).then( response => {
 		// Load the response HTML into Cheerio for parsing
@@ -111,7 +117,9 @@ async function myHubScraper( messagePassed )
 		images.each( ( index, element ) => {
 			if ( imageCount >= 5 ) { return false; } // Exit loop if 5 images have been posted
             const imageUrl = $( element ).attr( 'data-src' );  // src
-            if ( imageUrl ) // Check image URL
+            if ( imageUrl && !imageUrl.startsWith( 'data:image/gif;base64' ) 
+			&& !imageUrl.startsWith( 'https://di.phncdn.com/www-static/images' )
+			&& !imageUrl.startsWith( 'https://ei.phncdn.com/www-static/images' ) ) // Check image URL
             {
                 //console.log( imageUrl + ' hello 2' );
 				// Send the image URL to Discord
